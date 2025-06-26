2f55cc671cb21c237c799f94c14449524e9cd2ae
Veha Veha <vehav00@gmail.com>
2025-06-26 20:06:48 +0700

    chore(rebase all from autogen): free errors in codes but still unstable yet

--- demodapk/__main__.py
+++ demodapk/__main__.py
@@ -1,4 +1,4 @@
-from autogen import main
+from demodapk.cli import main
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "demodapk = demodapk.autogen:main",
+            "demodapk = demodapk.cli:main",
         ],
     },
 )
